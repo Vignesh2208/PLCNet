842d387225b4f9c12897a051c4ae2ad224c202b3
VIGNESH <vig2208@gmail.com>
2016-02-16 01:25:46 -0600

    Removed untracked files

--- build/lib.linux-i686-2.7/awlsim/gui/icons/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import division, absolute_import, print_function, unicode_literals
-
-from awlsim.gui.icons.icons import *
--- build/lib.linux-i686-2.7/awlsimhw_pyprofibus/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import division, absolute_import, print_function, unicode_literals
-
-from awlsimhw_pyprofibus.main import *
